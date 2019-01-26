7eebd10c1fca7873db6a103c3c25f8fb1fe77c04
Damian Escobedo <damian050697@gmail.com>
2019-01-25 21:28:50 -0700

    added parameter option to give a list of layouts file

--- changeKeyboard.sh
+++ changeKeyboard.sh
@@ -1,8 +1,37 @@
-
-currentKey=$(setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}')
-if [[ "$currentKey" == "us" ]]
+set -e
+currentLayout=$(setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}')
+if [[ $# == 1 ]]
 then
-  $(setxkbmap latam)
+  readarray langs < $1
+  flag=0
+  for lang in ${langs[@]}
+  do
+    echo "$lang"
+    if [[ $flag == 1 ]]
+   then
+      $(setxkbmap "$lang")
+      exit 0
+    else
+      if [[ "$currentLayout" == "$lang" ]]
+      then
+        flag=1
+      fi
+    fi
+  done
+  #if the script is still running that means the current layout isn't
+  #in the file or is the last one, so we just set it to the first element in the list
+  $(setxkbmap "${langs[0]}")
+  exit 0
 else
-  $(setxkbmap us)
+  if [[ $# == 0 ]]
+  then
+    if [[ "$currentLayout" == "us" ]]
+    then
+      $(setxkbmap latam)
+    else
+      $(setxkbmap us)
+    fi
+  else
+    echo "This script can only take 0 or 1 parameters"
+  fi
 fi
